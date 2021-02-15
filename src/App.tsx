bd6a193c39f5271d6dd56aa5ce6ec8bf9c461915
Reeska <thchampi@gmail.com>
2021-02-15 23:22:16 +0100

    Add Toggle component from Atlaskit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Toggle from '@atlaskit/toggle';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          Display a toggle <Toggle />
+        </div>
       </header>
     </div>
   );
